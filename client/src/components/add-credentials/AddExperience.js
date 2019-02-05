cbac1dc958b81d2aaaaa9e65693c210130930610
webstar0316 <webstar0316@outlook.com>
2019-02-06 08:56:19 +1000

    Add Experience Functionality

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,6 +4,7 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { addExperience } from "../../actions/profileActions";
 
 class AddExperience extends Component {
   constructor(props) {
@@ -24,14 +25,29 @@ class AddExperience extends Component {
     this.onCheck = this.onCheck.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
   onSubmit(e) {
     e.preventDefault();
+    const expData = {
+      company: this.state.company,
+      title: this.state.title,
+      location: this.state.location,
+      from: this.state.from,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description
+    };
 
-    console.log("submit");
+    this.props.addExperience(expData, this.props.history);
   }
 
   onCheck(e) {
@@ -112,18 +128,18 @@ class AddExperience extends Component {
                 </div>
                 <TextAreaFieldGroup
                   placeholder="Job Description"
-                  name="bio"
+                  name="description"
                   value={this.state.description}
                   onChange={this.onChange}
                   error={errors.description}
                   info="Tell us about the position"
                 />
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block mt-4"
+                />
               </form>
-              <input
-                type="submit"
-                value="Submit"
-                className="btn btn-info btn-block mt-4"
-              />
             </div>
           </div>
         </div>
@@ -133,6 +149,7 @@ class AddExperience extends Component {
 }
 
 AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
 };
@@ -142,4 +159,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(
+  mapStateToProps,
+  { addExperience }
+)(withRouter(AddExperience));
