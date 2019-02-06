69f5028bcf078fac27948c9853880c98c93d52a0
webstar0316 <webstar0316@outlook.com>
2019-02-06 10:18:10 +1000

    Dashboard experience display & delete

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -73,6 +73,24 @@ export const addExperience = (expData, history) => dispatch => {
     );
 };
 
+// Delete experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Add education
 export const addEducation = (eduData, history) => dispatch => {
   axios
