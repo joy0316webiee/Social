8d66152d16f1aea3161b1438fab852eb98ba8e29
webstar0316 <webstar0316@outlook.com>
2019-02-06 10:35:16 +1000

    Dashboard Education Display & Delete

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -91,6 +91,24 @@ export const deleteExperience = id => dispatch => {
     );
 };
 
+// Delete education
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Add education
 export const addEducation = (eduData, history) => dispatch => {
   axios
